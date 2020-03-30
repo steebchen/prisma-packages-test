54e76ca11d4c16e5739b64f588d1645c90990adf
Jan Piotrowski <piotrowski+github@gmail.com>
2020-03-31 01:24:12 +0200

    no message

--- prisma/1.sh
+++ prisma/1.sh
@@ -2,10 +2,14 @@
 
 set -eu
 
+set DEBUG=*
+
+echo "== npm install -g prisma =="
 npm install -g prisma
 echo ""
 echo ""
 echo "======================="
 echo ""
 echo ""
+echo "== npm install -g @prisma/cli@alpha =="
 npm install -g @prisma/cli@alpha
--- prisma2/1.sh
+++ prisma2/1.sh
@@ -2,10 +2,14 @@
 
 set -eu
 
+set DEBUG=*
+
+echo "== npm install -g prisma2 =="
 npm install -g prisma2
 echo ""
 echo ""
 echo "======================="
 echo ""
 echo ""
+echo "== npm install -g @prisma/cli@alpha =="
 npm install -g @prisma/cli@alpha
