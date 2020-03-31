842b7b33e817211adb9f78f3c17ee8c343989aca
Jan Piotrowski <piotrowski+github@gmail.com>
2020-03-31 02:25:08 +0200

    p2 5-8

---
+++ prisma2/5.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+export DEBUG=*
+
+npm init -y
+
+command1="npm install prisma2@2.0.0-preview023"
+command2="npm install -g @prisma/cli@alpha"
+
+echo "== $command1 =="
+$command1
+echo ""
+echo ""
+echo "======================="
+echo ""
+echo ""
+echo "== $command2 =="
+$command2
+
+echo ""
+echo ""
+echo "======================="
+echo ""
+echo ""
+echo "== prisma2 -v =="
+prisma2 -v
+echo "== npx prisma -v =="
+npx prisma -v
+echo "== npx prisma2 -v =="
+npx prisma2 -v
